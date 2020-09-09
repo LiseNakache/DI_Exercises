77a8a58c2a324ff92ab38d1c82ff1729cfd19a86
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-09-09 21:53:41 +0300

    changes to w5 d4 daily challenge completed

--- W5/D4/daily_challenge/daily_challenge.py
+++ W5/D4/daily_challenge/daily_challenge.py
@@ -1,9 +1,14 @@
+from gensim.parsing.preprocessing import remove_stopwords
 import re
+import string
 
 
 class Text:
     def __init__(self, string=""):
-        self.string = string
+        if string == "" or None:
+            Text.from_txt_file('the_stranger.txt')
+        else:
+            self.string = string
 
     def get_word_freq(self, word):
         """
@@ -22,7 +27,7 @@ class Text:
             ValueError(f"This word is not in here {word}")
 
     def word_list(self):
-        """:return strips dots and commas and returns list of words in string."""
+        """:return strips dots and commas and special characters and returns list of words in string."""
         try:
             self.string = re.sub('[^A-Za-z0-9]+', ' ', self.string)
             word_list = self.string.split()
@@ -53,15 +58,17 @@ class Text:
             # for each word in the list, check if already exists,
             # if not add to count_dict, if yes increment value in count dict
             for word in word_list:
-                if word not in count_dict:
-                    count_dict.update({word: 1})
-                else:
-                    count_dict[word] += 1
-                    if count_dict.get(word) > most_common_count:
-                        count_dict.update({"most_common": word})
+                if word.isalpha():
+                    if word not in count_dict:
+                        count_dict.update({word: 1})
+                    else:
+                        count_dict[word] += 1
+                        if count_dict.get(word) > most_common_count:
+                            count_dict.update({"most_common": word})
         except ValueError:
             raise ValueError
-        return count_dict.get("most_common")
+        most_common = count_dict.get("most_common")
+        return most_common
 
     def create_txt(self, filename):
         """
@@ -92,18 +99,46 @@ class Text:
             raise FileNotFoundError
 
 
+class TextModification(Text):
+    def __init__(self, string=""):
+        if string == "" or None:
+            Text.from_txt_file('the_stranger.txt')
+        else:
+            self.string = string
+        super().__init__(string)
+
+    def remove_special_ch(self):
+        """:return returns string of words in self.string without special characters."""
+        try:
+            self.string = re.sub('[^A-Za-z0-9]+', ' ', self.string)
+        except ValueError:
+            raise ValueError("Oh OOHHH")
+        return self.string
+
+    def remove_punctuation(self):
+        """:return returns self.string stipped of punctuation"""
+        self.string = self.string.strip(string.punctuation)
+        return self.string
+
+    def remove_stop_words(self):
+        """ method that returns the text without english stop-words
+        :returns string withour stop words"""
+        self.string = remove_stopwords(self.string)
+        return self.string
 
 def main():
-    text1 = Text("Bla baksdba bald asdlk asd asba asd asd a sa asd")
-    print(f"frequency of 'text1' in text1 {text1.get_word_freq('Bla')}")
-    print(f"most common word: {text1.get_most_common()}")
-    print(f"Word is unique list {text1.word_is_unique()}")
-    Text.from_txt_file('the_stranger.txt')
+    # text1 = Text("Bla baksdba bald asdlk asd asba asd asd a sa asd")
+    # print(f"frequency of 'text1' in text1 {text1.get_word_freq('Bla')}")
+    # print(f"most common word: {text1.get_most_common()}")
+    # print(f"Word is unique list {text1.word_is_unique()}")
+    # Text.from_txt_file('the_stranger.txt')
     text2 = Text()
-    print(f"frequency of 'and' in textfile {text2.get_word_freq('and')}")
+    # print(f"frequency of 'and' in textfile {text2.get_word_freq('and')}")
     print(f"most common word: {text2.get_most_common()}")
-    print(f"Word is unique list {text2.word_is_unique()}")
-
+    # # print(f"Word is unique list {text2.word_is_unique()}")
+    # text3 = TextModification()
+    # # print(text3.remove_punctuation())
+    # print(text3.remove_stop_words())
 
 if __name__ == '__main__':
     main()
